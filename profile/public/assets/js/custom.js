833053ed603d855fe55843c0b3b45739673c6e14
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2022-01-19 12:49:44 +0530

    Glightbox updated

---
+++ profile/public/assets/js/custom.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+    'href': 'https://www.youtube.com/watch?v=GStqMCW0B5g',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    'autoPlayVideos': 'true'
+});
